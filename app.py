d29a318e864a840661123834dac276bda9ef729e
andersonjackc <60368246+andersonjackc@users.noreply.github.com>
2021-11-13 23:20:15 -0500

    Update to RNG

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return "Look at this! Its a random number: " + str(random.randrange(0, 99999999999, 3))
+    return "Look at this! Its a random number: " + str(random.randrange(0, 99999999999))
 
 
 if __name__ == '__main__':
