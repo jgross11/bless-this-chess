75b727ef1bbe3e3b16d394e3cdbffd942924bf9d
jgross11 <jgross11@ycp.edu>
2021-11-19 22:23:39 -0500

    jinja2 tests

--- app.py
+++ app.py
@@ -3,29 +3,43 @@ import random
 
 import CredentialLoader
 import DBConnector
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 app = Flask(__name__)
 
 credentials = {}
 
 dbConnection = None
+env = None
 
 @app.route('/')
 def hello_world():  # put application's code here
     return "Look at this! Its a random number: " + str(random.randrange(0, 99999999999))
 
+@app.route('/template')
+def render():
+    template = env.get_template('test.html')
+    return template.render(var1='hello', var2='world!')
 
 @app.route('/test')
 def test():  # put application's code here
     return "would you look at that, a new page!"
 
 if __name__ == '__main__':
+
     credentials = CredentialLoader.load()
-    DBConnector.openConnection(
-        credentials[CredentialLoader.MYSQL_USERNAME],
-        credentials[CredentialLoader.MYSQL_ROOT_PASSWORD],
-        credentials[CredentialLoader.MYSQL_HOST],
-        credentials[CredentialLoader.MYSQL_DB],
-    )
-    DBConnector.closeConnection()
-    app.run()
+    if credentials == None:
+        print("No credentials = no soup for you!")
+    else:
+        DBConnector.openConnection(
+            credentials[CredentialLoader.MYSQL_USERNAME],
+            credentials[CredentialLoader.MYSQL_ROOT_PASSWORD],
+            credentials[CredentialLoader.MYSQL_HOST],
+            credentials[CredentialLoader.MYSQL_DB],
+        )
+        DBConnector.closeConnection()
+        env = Environment(
+            loader=PackageLoader('app', 'templates'),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+        app.run()
