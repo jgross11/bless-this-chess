2c5d69fd5a2e348d4aeb893007f402deb5882303
Josh G <47704419+jgross11@users.noreply.github.com>
2022-04-13 21:44:43 -0500

    Update Game.js
    basic piece move definitions and validation

--- bless_this_chess/view/static/js/Game.js
+++ bless_this_chess/view/static/js/Game.js
@@ -25,30 +25,250 @@ Game = () => {
             {}, // :)                   // empty square representation
             {                           // currently pieces are just names (used for sprite fetching)
                 name: "pawn",           // and "moves" array
-                moves: [],
+                moves: [{
+                    type: 0,             // 0 = offset (one time movement), 1 = pattern (row movement)
+                    dx: 0, 
+                    dy: 1
+                }],
             },
             {
                 name: "rook",
-                moves: [],
+                moves: [
+                    {
+                        type: 1,
+                        dx: 1,
+                        dy: 0,
+                    },
+                    {
+                        type: 1,
+                        dx: -1,
+                        dy: 0
+                    },
+                    {
+                        type: 1,
+                        dx: 0,
+                        dy: 1,
+                    },
+                    {
+                        type: 1,
+                        dx: 0,
+                        dy: -1
+                    }
+                ],
             },
             {
                 name: "knight",
-                moves: [],
+                moves: [
+                    {
+                        type: 0,
+                        dx: 1, 
+                        dy: 2
+                    },
+                    {
+                        type: 0,
+                        dx: 1, 
+                        dy: -2
+                    },
+                    {
+                        type: 0,
+                        dx: -1, 
+                        dy: 2
+                    },
+                    {
+                        type: 0,
+                        dx: -1, 
+                        dy: -2
+                    },
+                    {
+                        type: 0,
+                        dx: 2, 
+                        dy: 1
+                    },
+                    {
+                        type: 0,
+                        dx: -2, 
+                        dy: 1
+                    },
+                    {
+                        type: 0,
+                        dx: 2, 
+                        dy: -1
+                    },
+                    {
+                        type: 0,
+                        dx: -2, 
+                        dy: -1
+                    }
+                ],
             },
             {
                 name: "bishop",
-                moves: [],
+                moves: [
+                    {
+                        type: 1,
+                        dx: 1,
+                        dy: 1
+                    },
+                    {
+                        type: 1,
+                        dx: -1,
+                        dy: 1
+                    },
+                    {
+                        type: 1,
+                        dx: 1,
+                        dy: -1
+                    },
+                    {
+                        type: 1,
+                        dx: -1,
+                        dy: -1
+                    }
+                ],
             },
             {
                 name: "queen",
-                moves: [],
+                moves: [
+                    {
+                        type: 1,
+                        dx: 1,
+                        dy: 1
+                    },
+                    {
+                        type: 1,
+                        dx: -1,
+                        dy: 1
+                    },
+                    {
+                        type: 1,
+                        dx: 1,
+                        dy: -1
+                    },
+                    {
+                        type: 1,
+                        dx: -1,
+                        dy: -1
+                    },
+                    {
+                        type: 1,
+                        dx: 1,
+                        dy: 0,
+                    },
+                    {
+                        type: 1,
+                        dx: -1,
+                        dy: 0
+                    },
+                    {
+                        type: 1,
+                        dx: 0,
+                        dy: 1,
+                    },
+                    {
+                        type: 1,
+                        dx: 0,
+                        dy: -1
+                    }
+                ],
             },
             {
                 name: "king",
-                moves: [],
+                moves: [
+                    {
+                        type: 0,
+                        dx: 1,
+                        dy: 0
+                    },
+                    {
+                        type: 0,
+                        dx: 1,
+                        dy: 1
+                    },
+                    {
+                        type: 0,
+                        dx: 0,
+                        dy: 1
+                    },
+                    {
+                        type: 0,
+                        dx: -1,
+                        dy: 1
+                    },
+                    {
+                        type: 0,
+                        dx: -1,
+                        dy: 0
+                    },
+                    {
+                        type: 0,
+                        dx: -1,
+                        dy: -1
+                    },
+                    {
+                        type: 0,
+                        dx: 0,
+                        dy: -1
+                    },
+                    {
+                        type: 0,
+                        dx: 1,
+                        dy: -1
+                    }
+                ],
             },
         ],
 
+        // determine if x and y are within bounds of (assumed filled rectangular) board
+        coordsInBoard(x,y){ 
+            return 0 <= x && x < this.dimensions.width && 0 <= y && y < this.dimensions.height
+        },
+
+        isValidMove(pieceX, pieceY, newX, newY){
+            // get type of piece at this position
+            let pieceIndex = this.board[pieceY][pieceX];
+
+            // can't move an empty tile
+            if(pieceIndex === 0) return false;
+
+            // get the piece information
+            let piece = this.pieces[Math.abs(pieceIndex)];
+
+            // for each move of this piece
+            for(let move of piece.moves){
+
+                // add dx, dy to determine new tile position for this move
+                let newPieceX = pieceX + move.dx;
+                let newPieceY = pieceY + move.dy;
+
+                // if a one-off move (pawn, king, knight)
+                if(move.type == 0) {
+                    if(newPieceX == newX && newPieceY == newY){
+                        return true;
+                    }
+                }
+                // if a rule-like move (bishop, queen, rook)
+                else if(move.type == 1){
+                    let multiplier = 1;
+
+                    // move along the formulaic path described by this move rule until you're off the board
+                    // checking if the path lands on the selected tile
+                    while(this.coordsInBoard(newPieceX, newPieceY)){
+                        if(newPieceX == newX && newPieceY == newY)
+                            return true;
+                        else {
+                            multiplier++;
+                            newPieceX = pieceX + (move.dx * multiplier);
+                            newPieceY = pieceY + (move.dy * multiplier);
+                        }
+                    }
+                }
+            }
+            // if you get here, the piece cannot move to the targeted tile
+
+            // simulate move
+            // if game over rules hold
+            return false;
+        }
     }
     return obj;
 };
